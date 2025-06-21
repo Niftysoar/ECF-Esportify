f1488ec62e35d894492195065ca98910a58baff0
Mathéo Berthomé-Laurent <berthomath@hotmail.com>
2025-06-21 13:19:20 +0200

    Updated Config

--- pages/config.php
+++ pages/config.php
@@ -1,8 +1,14 @@
 <?php
-$host = 'mysql:host=db;port=3306';  // Hôte de la base de données
-$dbname = 'esports_db';  // Nom de la base de données
-$username = 'root';  // Utilisateur
-$password = 'supermotdepasse';  // Mot de passe
+// $host = 'mysql:host=db;port=3306';  // Hôte de la base de données
+// $dbname = 'esports_db';  // Nom de la base de données
+// $username = 'root';  // Utilisateur
+// $password = 'supermotdepasse';  // Mot de passe
+
+$pdo = new PDO(
+  "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8mb4",
+  getenv("DB_USER"),
+  getenv("DB_PASS")
+);
 
 try {
     $dsn = $host . ';dbname=' . $dbname;
