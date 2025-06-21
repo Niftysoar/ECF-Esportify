a12f5232ab9987b930d0b36b2cd90c8f7f1d0798
Mathéo Berthomé-Laurent <berthomath@hotmail.com>
2025-06-21 13:27:57 +0200

    Updated Config

--- pages/config.php
+++ pages/config.php
@@ -4,17 +4,31 @@
 // $username = 'root';  // Utilisateur
 // $password = 'supermotdepasse';  // Mot de passe
 
-$pdo = new PDO(
-  "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8mb4",
-  getenv("DB_USER"),
-  getenv("DB_PASS")
-);
+// $pdo = new PDO(
+//   "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8mb4",
+//   getenv("DB_USER"),
+//   getenv("DB_PASS")
+// );
+
+// try {
+//     $dsn = $host . ';dbname=' . $dbname;
+//         $pdo = new PDO($dsn, $username, $password);
+//         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Erreur de connexion : " . $e->getMessage());
+// }
 
 try {
-    $dsn = $host . ';dbname=' . $dbname;
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "pgsql:host=" . getenv("DB_HOST") . 
+           ";port=" . getenv("DB_PORT") . 
+           ";dbname=" . getenv("DB_NAME");
+
+    $pdo = new PDO($dsn, getenv("DB_USER"), getenv("DB_PASS"));
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
